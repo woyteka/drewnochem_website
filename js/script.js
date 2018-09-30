079847036fc233e54563c35f09acf3ca916226d8
woyteka <40150050+woyteka@users.noreply.github.com>
2018-09-30 06:46:15 +0200

    A06-menu-02

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.onload= function(){
+
+
+let btn= document.querySelector('.header-menu-hamburger');
+
+btn.addEventListener('click',function(){
+    let menu= document.querySelector('.header-menu-resposive');
+    menu.classList.add('visibility');
+    
+    
+})
+    
+};
+    
