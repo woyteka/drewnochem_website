c987b7a42f726cabba1c34d1be05b057071c32b5
woyteka <40150050+woyteka@users.noreply.github.com>
2018-10-02 18:25:35 +0200

    A07-load-menu-02

--- js/script.js
+++ js/script.js
@@ -12,6 +12,68 @@ $(document).ready(function(){
 
 
 
+let uslugi = document.querySelector('.uslugi');
+        uslugi.addEventListener('click',function(){
+        
+   $('content').load('uslugi.html');
+ })  
+
+
+
+
+
+
+let sg = document.querySelector('.sg');
+        sg.addEventListener('click',function(){
+        
+   $('content').load('strona-glowna.html');
+ })
+
+
+
+ 
+
+ 
+let sgM = document.querySelector('.sgM');
+        sgM.addEventListener('click',function(){
+        
+   $('content').load('strona-glowna.html');
+            $('.header-menu-resposive').css('display','none') 
+ })
+
+
+
+let uslugiM = document.querySelector('.uslugiM');
+        uslugiM.addEventListener('click',function(){
+        
+   $('content').load('uslugi.html');
+       $('.header-menu-resposive').css('display','none')     
+ }) 
+
+
+
+
+
+let realizacjeM = document.querySelector('.realizacjeM');
+        realizacjeM.addEventListener('click',function(){
+        
+   $('content').load('realizacje.html');
+            $('.header-menu-resposive').css('display','none') 
+ })
+
+let kontaktM = document.querySelector('.kontaktM');
+        kontaktM.addEventListener('click',function(){
+        
+   $('content').load('kontakt.html');
+            $('.header-menu-resposive').css('display','none') 
+ })
+
+
+
+
+
+
+
 
 
 
