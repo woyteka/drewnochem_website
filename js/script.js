6a6dfdb545e5837b14675ba2bb64db053e704b68
woyteka <40150050+woyteka@users.noreply.github.com>
2018-10-09 21:43:32 +0200

    A09-uslugi-02

--- js/script.js
+++ js/script.js
@@ -24,21 +24,76 @@ let sg = document.querySelector('.sg');
         
    $('content').load('strona-glowna.html');
  })
-
+ 
          
 
 let uslugi = document.querySelector('.uslugi');
       uslugi.addEventListener('click',function(){
   
    $('content').load('uslugi.html', function(){
+       
+        let obraz=document.querySelector('.content-right-img-box');
+              obraz.style.backgroundImage="url('images/kuchnia.jpg')";
+       
+       
+       
        let kuch = document.querySelector('.btn-kuchnie');
           kuch.addEventListener('click',function(){
               let obraz=document.querySelector('.content-right-img-box');
-            obraz.childNodes.removeChild;
-              obraz.style.backgroundColor="red";
+              obraz.style.backgroundImage="url('images/kuchnia.jpg')";
           
               
           });
+  
+       
+         let zabudow = document.querySelector('.btn-zabudowy');
+          zabudow.addEventListener('click',function(){
+                   
+              let obraz=document.querySelector('.content-right-img-box');
+              obraz.style.backgroundImage="url('images/zabudowy.jpg')";
+              
+          
+              
+          });
+       
+       
+       
+             let meble = document.querySelector('.btn-meble');
+          meble.addEventListener('click',function(){
+                   
+              let obraz=document.querySelector('.content-right-img-box');
+              obraz.style.backgroundImage="url('images/meble.jpg')";
+              
+          
+              
+          });
+       
+       
+       
+               let domy = document.querySelector('.btn-domy');
+          domy.addEventListener('click',function(){
+                   
+              let obraz=document.querySelector('.content-right-img-box');
+              obraz.style.backgroundImage="url('images/domy.jpg')";
+              
+          
+              
+          });
+       
+       
+             let ceramika = document.querySelector('.btn-ceramika');
+          ceramika.addEventListener('click',function(){
+                   
+              let obraz=document.querySelector('.content-right-img-box');
+              obraz.style.backgroundImage="url('images/ceramika.jpg')";
+              
+          
+              
+          });
+       
+       
+       
+       
    });
       
 
@@ -95,8 +150,137 @@ let sgM = document.querySelector('.sgM');
 let uslugiM = document.querySelector('.uslugiM');
         uslugiM.addEventListener('click',function(){
         
-   $('content').load('uslugi.html');
-       $('.header-menu-resposive').css('display','none')     
+   $('content').load('uslugi.html', function(){
+       
+        let obraz=document.querySelector('.content-right-img-box');
+              obraz.style.backgroundImage="url('images/kuchnia.jpg')";
+       
+       
+       
+       
+       
+       
+       
+      let kuch = document.querySelector('.btn-kuchnie');
+          kuch.addEventListener('click',function(){
+              let obraz=document.querySelector('.content-right-img-box');
+              obraz.style.backgroundImage="url('images/kuchnia.jpg')";
+          
+              
+          });
+  
+       
+         let zabudow = document.querySelector('.btn-zabudowy');
+          zabudow.addEventListener('click',function(){
+                   
+              let obraz=document.querySelector('.content-right-img-box');
+              obraz.style.backgroundImage="url('images/zabudowy.jpg')";
+              
+          
+              
+          });
+       
+       
+       
+             let meble = document.querySelector('.btn-meble');
+          meble.addEventListener('click',function(){
+                   
+              let obraz=document.querySelector('.content-right-img-box');
+              obraz.style.backgroundImage="url('images/meble.jpg')";
+              
+          
+              
+          });
+       
+       
+       
+               let domy = document.querySelector('.btn-domy');
+          domy.addEventListener('click',function(){
+                   
+              let obraz=document.querySelector('.content-right-img-box');
+              obraz.style.backgroundImage="url('images/domy.jpg')";
+              
+          
+              
+          });
+       
+       
+             let ceramika = document.querySelector('.btn-ceramika');
+          ceramika.addEventListener('click',function(){
+                   
+              let obraz=document.querySelector('.content-right-img-box');
+              obraz.style.backgroundImage="url('images/ceramika.jpg')";
+              
+          
+              
+          }); 
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+   });
+                    
+                    
+                   
+            
+            
+            
+       $('.header-menu-resposive').css('display','none')
+            
+            
+            
+            
+         
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
  }) 
 
 
