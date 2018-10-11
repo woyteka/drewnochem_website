1b02cc1366306afc37e0dd9e095808cf9b4d8f6a
woyteka <40150050+woyteka@users.noreply.github.com>
2018-10-11 12:02:14 +0200

    A09-uslugi-03

--- js/script.js
+++ js/script.js
@@ -42,6 +42,15 @@ let uslugi = document.querySelector('.uslugi');
               let obraz=document.querySelector('.content-right-img-box');
               obraz.style.backgroundImage="url('images/kuchnia.jpg')";
           
+              let tekstA =document.querySelector('.text-box-uslugi');
+              tekstA.innerHTML="Wykonujemy meble kuchenne pod wymiar. Na życzenie klienta przyjeżdżamy na pomiar przygotowujemy projekt, ustalamy rodzaj materiału oraz szacujemy koszt zabudowy.";
+              
+              
+              
+              
+              
+              
+              
               
           });
   
@@ -52,7 +61,9 @@ let uslugi = document.querySelector('.uslugi');
               let obraz=document.querySelector('.content-right-img-box');
               obraz.style.backgroundImage="url('images/zabudowy.jpg')";
               
-          
+          let tekstB =document.querySelector('.text-box-uslugi');
+              tekstB.innerHTML="Wykonujemy różnego rodzaju zabudowy z drzwiami przesuwanymi lub rozwieranymi. Przygotowujemy także gotowe elementy do samodzielnego montażu ";
+              
               
           });
        
@@ -64,7 +75,8 @@ let uslugi = document.querySelector('.uslugi');
               let obraz=document.querySelector('.content-right-img-box');
               obraz.style.backgroundImage="url('images/meble.jpg')";
               
-          
+            let tekstC =document.querySelector('.text-box-uslugi');
+              tekstC.innerHTML="Wzorujemy się na polskich meblach z lat 50-ch. Wykonane w 100% z drewna tworzą niepowtarzalny klimat.Idealnie komponują się w zarówno w starych kamienicach jaki i w nowoczesnych przestronnych wnętrzach.";
               
           });
        
@@ -76,7 +88,8 @@ let uslugi = document.querySelector('.uslugi');
               let obraz=document.querySelector('.content-right-img-box');
               obraz.style.backgroundImage="url('images/domy.jpg')";
               
-          
+          let tekstD =document.querySelector('.text-box-uslugi');
+              tekstD.innerHTML="Obtaczamy bale świerkowe lub sosnowe o średnicy od 15cm do 50cm i długości od 1 do 6m. Przygotowujemy także bale umożliwiające montaż domu z bali z frezem na siodło i zamek węgłowy. Wykonujemy małe domy z bali.";
               
           });
        
@@ -87,7 +100,8 @@ let uslugi = document.querySelector('.uslugi');
               let obraz=document.querySelector('.content-right-img-box');
               obraz.style.backgroundImage="url('images/ceramika.jpg')";
               
-          
+          let tekstE =document.querySelector('.text-box-uslugi');
+              tekstE.innerHTML="Firma współpracuje z regionalnymi artystami. Produkty z drewna i ceramiki można zakupić bezpośrednio u nas.Organizujemy zajęcia z ceramiki (lepienie z gliny) ";
               
           });
        
